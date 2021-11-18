fa15964958f7268c65a6e0b0629b378386f8f79d
MZO9400 <hamza.hameed00@yahoo.com>
2021-11-19 00:26:25 +0500

    Reformat and cleanup code

--- app/src/main/java/com/ccompany/client/HomeActivity.kt
+++ app/src/main/java/com/ccompany/client/HomeActivity.kt
@@ -1,8 +1,8 @@
 package com.ccompany.client
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
