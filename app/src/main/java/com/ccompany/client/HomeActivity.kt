f1258335dce636bcef4b7390804e3ee7093ede83
MZO9400 <hamza.hameed00@yahoo.com>
2021-11-19 01:00:10 +0500

    HomeActivity: Create buttons for opening maps and login screen

--- app/src/main/java/com/ccompany/client/HomeActivity.kt
+++ app/src/main/java/com/ccompany/client/HomeActivity.kt
@@ -2,16 +2,27 @@ package com.ccompany.client
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var mBtnLogin: Button
+    private lateinit var mBtnMaps: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val intent = Intent(this, LoginActivity::class.java)
-        startActivity(intent)
-    }
-    override fun onResume() {
-        super.onResume()
+
+        mBtnLogin = findViewById(R.id.login)
+        mBtnMaps = findViewById(R.id.open_in_maps)
+
+        mBtnLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+        mBtnMaps.setOnClickListener {
+            val intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
