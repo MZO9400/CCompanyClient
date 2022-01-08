a097e55b6e0fca6a301cad0218b044454ca9f7e9
MZO9400 <hamza.hameed00@yahoo.com>
2022-01-08 17:06:32 +0500

    AuthManager: Login and register APIs now use coroutines for simpler async execution.
    Remove callbacks, follow single responsibility

--- app/src/main/java/com/ccompany/service/AuthManager.kt
+++ app/src/main/java/com/ccompany/service/AuthManager.kt
@@ -1,59 +1,25 @@
 package com.ccompany.service
 
 import android.content.Context
-import android.util.Log
 import com.ccompany.interfaces.LoginRequest
 import com.ccompany.interfaces.LoginResponse
 import com.ccompany.interfaces.RegisterRequest
 import com.ccompany.interfaces.RegisterResponse
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 
 class AuthManager(context: Context) {
     private var mContext: Context? = context
 
-    fun login(username: String, password: String) {
-        val client = APIClient.client.create(APIInterface::class.java)
-        val request: Call<LoginResponse> = client.login(LoginRequest(username, password))
-        request.enqueue(object : Callback<LoginResponse> {
-            override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
-                if (response.isSuccessful) {
-                    val loginResponse = response.body()
-                    if (loginResponse != null) {
-                        if (loginResponse.status) {
-                            saveToken(loginResponse.token)
-                        }
-                    }
-                }
-            }
-
-            override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
-                Log.e("AuthManager", "Login response: $t")
-            }
-        })
+    suspend fun login(username: String, password: String): LoginResponse {
+        return APIClient.client
+            .create(APIInterface::class.java)
+            .login(LoginRequest(username, password))
     }
 
-    fun register(username: String, password: String, name: String) {
-        val client = APIClient.client.create(APIInterface::class.java)
-        val request: Call<RegisterResponse> = client.register(RegisterRequest(username, password, name))
-        request.enqueue(object : Callback<RegisterResponse> {
-            override fun onResponse(call: Call<RegisterResponse>, response: Response<RegisterResponse>) {
-                if (response.isSuccessful) {
-                    val registerResponse = response.body()
-                    if (registerResponse != null) {
-                        if (registerResponse.status) {
-                            saveToken(registerResponse.token)
-                        }
-                    }
-                }
-            }
-
-            override fun onFailure(call: Call<RegisterResponse>, t: Throwable) {
-                Log.e("AuthManager", "Register response: $t")
-            }
-        })
+    suspend fun register(username: String, password: String, name: String): RegisterResponse {
+        return APIClient.client
+            .create(APIInterface::class.java)
+            .register(RegisterRequest(username, password, name))
     }
 
     fun logout(): Boolean {
