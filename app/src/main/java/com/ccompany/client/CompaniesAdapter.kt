ce626d9d9daa806d805ca775646cae49c3cea906
MZO9400 <hamza.hameed00@yahoo.com>
2022-01-09 19:41:06 +0500

    Clean up interface for each row

--- app/src/main/java/com/ccompany/client/CompaniesAdapter.kt
+++ app/src/main/java/com/ccompany/client/CompaniesAdapter.kt
@@ -18,13 +18,11 @@ import com.ccompany.interfaces.Company
 class CompaniesAdapter(private val companiesList: List<Company>) : RecyclerView.Adapter<CompanyViewHolder>() {
     class CompanyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         var name: TextView
-        var description: TextView
         var image: ImageView
         var phone: ImageView
 
         init {
             name = view.findViewById(R.id.name)
-            description = view.findViewById(R.id.description)
             image = view.findViewById(R.id.image)
             phone = view.findViewById(R.id.phone)
         }
@@ -38,7 +36,6 @@ class CompaniesAdapter(private val companiesList: List<Company>) : RecyclerView.
     override fun onBindViewHolder(holder: CompanyViewHolder, position: Int) {
         val company = companiesList[position]
         holder.name.text = company.name
-        holder.description.text = company.description
         val imageBytes = Base64.decode(company.logo.split(',')[1], Base64.DEFAULT)
         val decodedImage = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
         holder.image.setImageBitmap(decodedImage)
